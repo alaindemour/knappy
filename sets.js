e9c4ab8f7536653c42a1b4715a1044d8db664c5c
alain demour <alaindemour@gmail.com>
2015-12-27 19:09:21 -0500

    cleanup

--- sets.js
+++ sets.js
@@ -7,52 +7,53 @@
 const _ = require('lodash')
 
 
-let sampleItem = {benefit: 4, cost : 2}
+let sampleItem = {benefit: 4, cost: 2}
 
 // generates all subset of element of a given size, from a given array, but only starting at some position
-function allSubSetsOfSize(fromArr,subsetSize,startAt) {
+function allSubSetsOfSize(fromArr, subsetSize, startAt) {
 
-  let subsize = (typeof subsetSize  !== 'undefined') ? subsetSize : 1
-  let at = (typeof startAt  !== 'undefined') ? startAt : 0
+  let subsize = (typeof subsetSize !== 'undefined') ? subsetSize : 1
+  let at = (typeof startAt !== 'undefined') ? startAt : 0
+  let len = fromArr.length
+
 
   console.log(`fromArr ${fromArr} subsetSize ${subsize}  startAt ${at}`)
 
-  if (subsize > fromArr.length - at){
+  // Base Cases
+  if (subsize > len - at) {
     return []
   }
-
   if (subsize === 0) {
     return []
   }
-
   if (subsize === 1) {
     // TBD replace with some immutable collection module
     let result = []
-    for (let i = at; i < fromArr.length;i++){
+    for (let i = at; i < len; i++) {
       result.push([fromArr[i]])
     }
     return result
   }
-
-  if (subsetSize === fromArr.length - startAt) {
+  if (subsetSize === len - at) {
     return fromArr.slice(at)
   }
 
+  // General Case
   let result = []
-  for (let i = at ; i < fromArr.length; i++) {
-    for (let j = i+1; j < fromArr.length; j++){
-      let reduced = allSubSetsOfSize(fromArr,subsize- 1,j)
-      for (let k = 0; k < reduced.length; k++) {
-        let e = [fromArr[i]].concat(reduced[k])
-        result.push(e)
-      }
-      console.log("key")
+  for (let i = at; i < len; i++) {
+    let current = fromArr[i]
+    let reduced = allSubSetsOfSize(fromArr, subsize - 1, i+1)
+    let reducedLen = reduced.length
+    for (let k = 0; k < reducedLen; k++) {
+      let e = [current].concat(reduced[k])
+      result.push(e)
     }
+    console.log("key")
   }
   return result
 }
 
 
 module.exports = {
-  allSubSetsOfSize : allSubSetsOfSize
+  allSubSetsOfSize: allSubSetsOfSize
 }
