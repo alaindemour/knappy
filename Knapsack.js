6c663b330720b0111da13c3d5387554c10b7e1b7
alaindemour <alaindemour@gmail.com>
2016-01-10 12:43:44 -0500

    snapshot

--- Knapsack.js
+++ Knapsack.js
@@ -9,113 +9,111 @@ const _ = require('lodash')
 
 
 class Knapsack {
-  constructor(params) {
-    this.capacity = params.capacity
-  }
-
-  zeroOneKnapsackRecursiveMemo(listOfItems) {
-
-    let capacity = this.capacity
-    let numberOfPicks = listOfItems.length
-    // position zero unused in this memo table, just to keep the mapping clear and obivious
-    let memo = []
-    for (let i = 0; i <= listOfItems.length; i++) {
-      memo[i] = []
-    }
-    let memoHits = 0
-    let complexity = 0
-
-    function bestMax(capacity, numberOfPicks) {
-
-      let hit = memo[numberOfPicks][capacity]
-      if (hit) {
-        memoHits++
-        return hit
-      }
-
-      complexity++
-      // Base case stops the recursion
-      if (numberOfPicks === 0 || capacity === 0) {
-        let result = [{cumul: 0, item: null}]
-        memo[numberOfPicks][capacity] = result
-        return result
-      }
-
-      // General recursive case
-      let currentItem = listOfItems[numberOfPicks - 1]
-
-      if (currentItem.cost > capacity) {
-        return bestMax(capacity, numberOfPicks - 1)
-      }
-      let A = bestMax(capacity - currentItem.cost, numberOfPicks - 1)
-      let B = bestMax(capacity, numberOfPicks - 1)
-      let pathAbenefit = A[0].cumul + currentItem.benefit
-      let pathBbenefit = B[0].cumul
-
-      let result
-      if (pathBbenefit > pathAbenefit) {
-        result = B
-      }
-      else {
-        result = [{cumul: pathAbenefit, item: currentItem}].concat(A)
-        memo[numberOfPicks][capacity] = result
-      }
-      return result
+    constructor(params) {
+        this.capacity = params.capacity
     }
 
-    let finalSolution = bestMax(capacity, numberOfPicks)
-    console.log(`Memoized Hits: ${memoHits}`)
-    console.log(`recursive complexity: ${complexity}`)
-    console.log(`bottom up complexity: ${listOfItems.length * capacity}`)
-    return finalSolution
-  }
+    zeroOneKnapsackRecursiveMemo(listOfItems) {
 
-
-  zeroOneKnapsackRecursive(listOfItems) {
-
-    let capacity = this.capacity
-    let numberOfPicks = listOfItems.length
-
-    function bestMax(capacity, numberOfPicks) {
-      // Base case stops the recursion
-      if (numberOfPicks === 0 || capacity === 0) {
-        return [{cumul: 0, item: null}]
-      }
-
-      // General recursive case
-      let currentItem = listOfItems[numberOfPicks - 1]
-      if (currentItem.cost > capacity) {
-        return bestMax(capacity, numberOfPicks - 1)
-      }
-
-      let A = bestMax(capacity - currentItem.cost, numberOfPicks - 1)
-      let B = bestMax(capacity, numberOfPicks - 1)
-      let pathAbenefit = A[0].cumul + currentItem.benefit
-      let pathBbenefit = B[0].cumul
-
-      return   pathBbenefit > pathAbenefit ? B : [{cumul: pathAbenefit, item: currentItem}].concat(A)
-    }
-
-    return bestMax(capacity, numberOfPicks)
-
-  }
-
-
-  // brute for is simple if  very inefficient, to be used to unit testing the faster but more bug-prone versions
-  bruteForce(listOfItems) {
-    let max = new Item({benefit: 0, cost: 0})
-    let all = powerSet(listOfItems)
-    for (let sub of all) {
-      let candidate = Item.value(sub)
-      if (candidate.cost <= this.capacity) {
-        if (candidate.benefit > max.benefit) {
-          max = candidate
-          max.list = sub
+        let capacity = this.capacity
+        let numberOfPicks = listOfItems.length
+        // position zero unused in this memo table, just to keep the mapping clear and obivious
+        let memo = []
+        for (let i = 0; i <= listOfItems.length; i++) {
+            memo[i] = []
         }
-      }
+        let memoHits = 0
+        let complexity = 0
+
+        function bestMax(capacity, numberOfPicks) {
+
+            let hit = memo[numberOfPicks][capacity]
+            if (hit) {
+                memoHits++
+                return hit
+            }
+
+            complexity++
+            // Base case stops the recursion
+            if (numberOfPicks === 0 || capacity === 0) {
+                let result = [{cumul: 0, item: null}]
+                memo[numberOfPicks][capacity] = result
+                return result
+            }
+
+            // General recursive case
+            let currentItem = listOfItems[numberOfPicks - 1]
+
+            if (currentItem.cost > capacity) {
+                return bestMax(capacity, numberOfPicks - 1)
+            }
+            let A = bestMax(capacity - currentItem.cost, numberOfPicks - 1)
+            let B = bestMax(capacity, numberOfPicks - 1)
+            let pathAbenefit = A[0].cumul + currentItem.benefit
+            let pathBbenefit = B[0].cumul
+
+            let result
+            if (pathBbenefit > pathAbenefit) {
+                result = B
+            }
+            else {
+                result = [{cumul: pathAbenefit, item: currentItem}].concat(A)
+                memo[numberOfPicks][capacity] = result
+            }
+            return result
+        }
+
+        let finalSolution = bestMax(capacity, numberOfPicks)
+        console.log(`Memoized Hits: ${memoHits}`)
+        console.log(`recursive complexity: ${complexity}`)
+        console.log(`bottom up complexity: ${listOfItems.length * capacity}`)
+        return finalSolution
     }
-    return max
-  }
+
+
+    zeroOneKnapsackRecursive(listOfItems) {
+
+        let capacity = this.capacity
+        let numberOfPicks = listOfItems.length
+        return bestMax(capacity, numberOfPicks)
+
+        function bestMax(capacity, numberOfPicks) {
+            // Base case stops the recursion
+            if (numberOfPicks === 0 || capacity === 0) {
+                return [{cumul: 0, item: null}]
+            }
+
+            // General recursive case
+            let currentItem = listOfItems[numberOfPicks - 1]
+            if (currentItem.cost > capacity) {
+                return bestMax(capacity, numberOfPicks - 1)
+            }
+
+            let A = bestMax(capacity - currentItem.cost, numberOfPicks - 1)
+            let B = bestMax(capacity, numberOfPicks - 1)
+            let pathAbenefit = A[0].cumul + currentItem.benefit
+            let pathBbenefit = B[0].cumul
+
+            return pathBbenefit > pathAbenefit ? B : [{cumul: pathAbenefit, item: currentItem}].concat(A)
+        }
+    }
+
+
+    // brute for is simple if  very inefficient, to be used to unit testing the faster but more bug-prone versions
+    bruteForce(listOfItems) {
+        let max = new Item({benefit: 0, cost: 0})
+        let all = powerSet(listOfItems)
+        for (let sub of all) {
+            let candidate = Item.value(sub)
+            if (candidate.cost <= this.capacity) {
+                if (candidate.benefit > max.benefit) {
+                    max = candidate
+                    max.list = sub
+                }
+            }
+        }
+        return max
+    }
 
 }
 
