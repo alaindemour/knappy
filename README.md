814d851002fb35ff246fb883ba7fc315063a5b53
alaindemour <alaindemour@gmail.com>
2016-01-11 20:48:37 -0500

    added some details

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 Musings on knapsack problems.
 That's a recursive and memoized version of the 0 1 problem, which has better time and space complexity than the bottom up dynamic programming approach that is in all textbooks
 
+# Performance
+Interestingly enought this is one of the rare practical problem where we can use a version of the Y combinator:
+to memoize the highly recursive knapsack solution we can do it using a "memoizing" Y combinator, thus cleaning separating
+the problem logic from the the caching and memozing "plumbing".
+The Y combinator does not add much performance overhead here, the performance is really affected by how performant 
+the multi-dimenstional lookup table used for the cache is. A well sized and optimized one can have a 10x performance difference 
+with a totally dynamic one
+
+# node version
 To run the code you need node 5.4.0 or higher with the harmony flag on as the code uses ES6 rest parameters
 
---
+++ benchmarks/bench.js
@@ -0,0 +1,56 @@
+/**
+ * Created by alaindemour on 1/11/16.
+ */
+
+"use strict"
+
+const _ = require('lodash')
+const assert = require('assert')
+const sets = require('../powerSet')
+const allSubSetsOfSize = sets.allSubSetsOfSize
+const powerSet = sets.powerSet
+const Knapsack = require('../Knapsack')
+const Item = require('../Item')
+const TrieCache = require('../TrieCache')
+const pseudoY = require('../Ycombinator').pseudoY
+const memoY = require('../Ycombinator').memoY
+
+
+let smallerKnapsack = new Knapsack({capacity: 30})
+
+let item1 = new Item({benefit: 60, cost: 10, name: 'item1'})
+let item2 = new Item({benefit: 100, cost: 20, name: 'item2'})
+let item3 = new Item({benefit: 120, cost: 30, name: 'item3'})
+let item4 = new Item({benefit: 90, cost: 10, name: 'item4'})
+let item5 = new Item({benefit: 75, cost: 13, name: 'item5'})
+let item6 = new Item({benefit: 110, cost: 10, name: 'item6'})
+
+let sextet = [item1, item2, item3, item4, item5, item6]
+let before = 0
+let after = 0
+
+
+before = Date.now()
+let bruteForceResult = smallerKnapsack.bruteForce(sextet)
+after = Date.now()
+console.log(`Brute Force took ${after - before}  msec`)
+
+before = Date.now()
+let naiveRecursiveResult = smallerKnapsack.zeroOneKnapsackRecursive(sextet)
+after = Date.now()
+console.log(`Naive Recursive took ${after - before}  msec`)
+
+before = Date.now()
+let handMemoizedRecursiveResult = smallerKnapsack.zeroOneKnapsackRecursiveMemo(sextet)
+after = Date.now()
+console.log(`Hand Memoized Recursive took ${after - before}  msec`)
+
+before = Date.now()
+let YRecursiveResult = smallerKnapsack.zeroOneKnapsackRecursiveY(sextet)
+after = Date.now()
+console.log(`Y combinator recursive took ${after - before}  msec`)
+
+before = Date.now()
+let YMemoizedRecursiveResult = smallerKnapsack.zeroOneKnapsackRecursiveMemoY(sextet)
+after = Date.now()
+console.log(`Y combinator memoized recursive took ${after - before}  msec`)
